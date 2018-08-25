c6e31a5ceb44d410e397f320f88739de84751214
MihailPolyakov <32265787+MihailPolyakov@users.noreply.github.com>
2018-08-25 12:23:52 +1000

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,4 @@
 <?php	
-var_dump($_GET);
 if (!empty($_GET)) {
 	$array=0;
 	$newarray=[];
@@ -11,7 +10,8 @@ if (!empty($_GET)) {
 	if (array_key_exists($_GET['number'], $arrayfiles) == true) {
 		$int = $_GET['number'];
 		if ($arrayfiles[$int]=='.' || $arrayfiles[$int]== '..'){
-			/*http_response_code(404);*/
+			http_response_code(404);
+			exit;
 		} else {
 			$fl=$arrayfiles[$int];
 			$test = file_get_contents("exams/$fl");
@@ -20,37 +20,38 @@ if (!empty($_GET)) {
 			?>
 
 			<form action="" method="POST">
-			<?php 			        				        
-				foreach ($array as $key => $value) {
-					++$qcount;
-					$i = 0;
-					?>
-				  <fieldset>
-					<legend> <?php echo $key ?></legend>
-					<?php foreach ($value as $asks) {
-						if (is_array($asks)){		
-								foreach ($asks as $value) {	
-									++$i;?>			            						 
-					<label><input type="radio" name="q<?php echo $qcount?>" value = "<?php echo $i?>"> <?php echo $value ?></label>
+		<?php 			        				        
+			foreach ($array as $key => $value) {
+				++$qcount;
+				$i = 0;
+				?>
+			  <fieldset>
+			    <legend> <?php echo $key ?></legend>
+			    <?php foreach ($value as $asks) {
+				if (is_array($asks)){		
+						foreach ($asks as $value) {	
+							++$i;?>			            						 
+			    <label><input type="radio" name="q<?php echo $qcount?>" value = "<?php echo $i?>"> <?php echo $value ?></label>
 
-					<?php  }
-						 } elseif (!is_array($asks)) {
-							 $newarray[]=$asks;
-						 }
+			    <?php  }
+				 } elseif (!is_array($asks)) {
+					 $newarray[]=$asks;
+				 }
 
-						}?>	  
-				  </fieldset> 
-				<?php }
+				}?>	  
+			  </fieldset> 
+			<?php }
 
-				}?>
-				<div><input type="text" name="name" placeholder="Введите свое имя"></div>
-			  <input type="submit" value="Отправить">  
-			</form>
-
-<?php } else {
-		/*http_response_code(404);*/
-	}
-};
+			}?>
+			<div><input type="text" name="name" placeholder="Введите свое имя"></div>
+		  <input type="submit" value="Отправить">  
+		</form>
+				
+	   <?php } else {
+		    http_response_code(404);
+	            exit;
+		}
+		};
 	
 	if (!empty($_POST)){
 	
@@ -73,16 +74,23 @@ if (!empty($_GET)) {
 	$image=imagecreatetruecolor(300, 150);
 	$textfront=imagecolorallocate($image, 100, 100, 100);
 	$background=imagecolorallocate($image, 150, 150, 150);
-	$imagefront=imagecreatefromjpeg(__DIR__ . '/image.jpg');
+	$im = __DIR__ . '/image.jpg';
+	if (!file_exists($im)) {
+		echo "Файл не найден";
+		exit;
+	}
+	$imagefront=imagecreatefromjpeg($im);
+	
 	$inimage=imagefill($image, 0, 0, $background);
 	imagecopy($image, $imagefront, 0, 0, 0, 0, 200, 120);
 	$fontfile=__DIR__ . '/12136.ttf';
+	if (!file_exists($fontfile)) {
+		echo "Файл не найден";
+		exit;
+	}
 	imagettftext($image, 16, 0, 10, 10, $textfront, $fontfile, $text);
-
-	header('Content-Type: image/png');
-
-	imagepng($image);
+	header('Content-Type: image/jpeg');
+	imagejpeg($image);
 	
 	}	
-	
 	
